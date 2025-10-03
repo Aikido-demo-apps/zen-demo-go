90d68ba936643473883b6122dae65f913e864242
BitterPanda <bitterpanda@proton.me>
2025-10-03 16:21:07 +0200

    Add main app main.go

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"zen-demo-go/database"
+	"zen-demo-go/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Initialize database
+	database.InitDatabase()
+
+	// HTML routes
+	r.GET("/", func(c *gin.Context) {
+		c.File("./static/index.html")
+	})
+
+	r.GET("/pages/create", func(c *gin.Context) {
+		c.File("./static/create.html")
+	})
+
+	r.GET("/pages/execute", func(c *gin.Context) {
+		c.File("./static/execute_command.html")
+	})
+
+	r.GET("/pages/read", func(c *gin.Context) {
+		c.File("./static/read_file.html")
+	})
+
+	r.GET("/pages/request", func(c *gin.Context) {
+		c.File("./static/request.html")
+	})
+
+	// Setup API routes
+	routes.SetupTestRoutes(r)
+	routes.SetupCommandRoutes(r)
+	routes.SetupRequestRoutes(r)
+	routes.SetupFileRoutes(r)
+	routes.SetupPetRoutes(r)
+
+	r.Static("/css", "./static/public/css")
+	r.Static("/js", "./static/public/js")
+	r.Static("/New-Grotesk", "./static/public/New-Grotesk")
+	r.GET("/aikido_logo.svg", func(c *gin.Context) {
+		c.File("./static/public/aikido_logo.svg")
+	})
+
+	// Start server
+	fmt.Println("Server is running on http://localhost:3000")
+	r.Run(":3000")
+}
